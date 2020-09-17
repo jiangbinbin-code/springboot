9983e3ff10ee5028602c5d737c6946d41838c522
9900007 <9900007@pactera.com>
2020-09-17 16:32:32 +0800

    commit

--- myspilder3/src/main/java/com/ghac/myspilder/myspilder3/util/HttpBaseUtil.java
+++ myspilder3/src/main/java/com/ghac/myspilder/myspilder3/util/HttpBaseUtil.java
@@ -1,46 +1,111 @@
 package com.ghac.myspilder.myspilder3.util;
 
-import cn.hutool.core.util.ReUtil;
 import cn.hutool.http.HttpUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
+import org.thymeleaf.util.StringUtils;
+import java.util.ArrayList;
 import java.util.List;
-
 public class HttpBaseUtil {
 
 
     public static void HttpGet(String url) {
         //请求列表页
         String listContent = HttpUtil.get("https://data.stats.gov.cn/");
-        System.out.println(listContent);
+        String preURl="https://data.stats.gov.cn/";
+        List<String> ifnormas=new ArrayList<>();
+        List<String> tables=new ArrayList<>();
+        List<String> easys=new ArrayList<>();
+        List<String> searchs=new ArrayList<>();
         Document doc = Jsoup.parse(listContent);
         Elements links = doc.select("a[href]");
         links.forEach(link -> {
             String linkHref = link.attr("href");
-            System.out.println(linkHref);
+            if(StringUtils.contains(linkHref,"ifnormal.htm")){
+                ifnormas.add(preURl+linkHref);
+            }else if(StringUtils.contains(linkHref,"tablequery.htm")){
+                tables.add(preURl+linkHref);
+            }else if(StringUtils.contains(linkHref,"search.htm")){
+                searchs.add(preURl+linkHref);
+            }else if(StringUtils.contains(linkHref,"easyquery.htm")){
+                easys.add(preURl+linkHref);
+            }else{
+              //  System.out.println("----"+linkHref);
+            }
         });
-
-//        List<String> strings = links.eachText();
-//        strings.forEach(link->{
-//            System.out.println(link);
-//        });
-        //使用正则获取所有标题
-        //System.out.println(listContent);
-//        List<String> titles = ReUtil.findAll("<span class=\"text-ellipsis\">(.*?)</span>", listContent, 1);
-//        for (String title : titles) {
-//            //打印标题
-//            System.out.println(""+title);
-//
-//        }
-//        List<String> all = ReUtil.findAll("<a\\b[^>]+\\bhref=\"([^\"]*)\"[^>]*>([\\s\\S]*?)</a>\n", listContent, 2);
-//        all.forEach(a->{
-//            System.out.println(a);
-//        });
+        System.out.println("---------------easys start -------------");
+        getEasyQueryInfo( easys);
+        System.out.println("---------------easys end -------------");
+        System.out.println("---------------searchs starts -------------");
+        getSearchQueryInfo( searchs);
+        System.out.println("---------------searchs end -------------");
+        System.out.println("---------------tables starts -------------");
+        getTablequeryInfo( tables);
+        System.out.println("---------------tables end -------------");
+        System.out.println("---------------ifnormas starts -------------");
+        getIfnormalInfo( ifnormas);
+        System.out.println("---------------ifnormas end -------------");
     }
 
     public static void main(String args[]){
         HttpGet("");
+    }
+
+    public static void getEasyQueryInfo(List<String> easyUrl){
+        if(null!=easyUrl && easyUrl.size()>0){
+            easyUrl.forEach(url->{
+                String listContent = HttpUtil.get(url);
+                Document doc = Jsoup.parse(listContent);
+                Elements links = doc.select("a[href]");
+                links.forEach(link -> {
+                    String linkHref = link.attr("href");
+                    System.out.println(linkHref);
+                });
+            });
+        }
+
+    }
+    public static void getSearchQueryInfo(List<String> searchUrl){
+        if(null!=searchUrl && searchUrl.size()>0){
+            searchUrl.forEach(url->{
+                String listContent = HttpUtil.get(url);
+                Document doc = Jsoup.parse(listContent);
+                Elements links = doc.select("a[href]");
+                links.forEach(link -> {
+                    String linkHref = link.attr("href");
+                    System.out.println(linkHref);
+                });
+            });
+        }
+
+    }
+    public static void getTablequeryInfo(List<String> tableUrl){
+        if(null!=tableUrl && tableUrl.size()>0){
+            tableUrl.forEach(url->{
+                String listContent = HttpUtil.get(url);
+                Document doc = Jsoup.parse(listContent);
+                Elements links = doc.select("a[href]");
+                links.forEach(link -> {
+                    String linkHref = link.attr("href");
+                    System.out.println(linkHref);
+                });
+            });
+        }
+
+    }
+    public static void getIfnormalInfo(List<String> infomalUrl){
+        if(null!=infomalUrl && infomalUrl.size()>0){
+            infomalUrl.forEach(url->{
+                String listContent = HttpUtil.get(url);
+                Document doc = Jsoup.parse(listContent);
+                Elements links = doc.select("a[href]");
+                links.forEach(link -> {
+                    String linkHref = link.attr("href");
+                    System.out.println(linkHref);
+                });
+            });
+        }
+
     }
 }
