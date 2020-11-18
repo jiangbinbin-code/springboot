b860819b0d4aed0f1a116580cc7f170b6a843b0a
jiangbinbin <712041476@qq.com>
2020-11-18 22:34:29 +0800

    删除不需要的文件

--- tushare/src/main/java/com/ghac/myspilder/myspilder3/TushareApplication.java
+++ tushare/src/main/java/com/ghac/myspilder/myspilder3/TushareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Myspilder3Application {
+public class TushareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Myspilder3Application.class, args);
+        SpringApplication.run(TushareApplication.class, args);
     }
 
 }
--- tushare/src/test/java/com/ghac/myspilder/myspilder3/HttpClientTest.java
+++ tushare/src/test/java/com/ghac/myspilder/myspilder3/HttpClientTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = com.ghac.myspilder.myspilder3.Myspilder3Application.class)
+@SpringBootTest(classes = TushareApplication.class)
 public class HttpClientTest {
 
 
